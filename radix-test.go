facab7e35848a5f375b7a806cebc8e5eadf8b749
yushizhao <yushizhao@outlook.com>
2019-06-27 03:45:56 +0100

    radix

--- radix-test.go
+++ radix-test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/mediocregopher/radix"
 )
@@ -20,12 +21,46 @@ func main() {
 		)
 	}
 
-	sentinel, err := radix.NewSentinel("mymaster", address, radix.SentinelConnFunc(customConnFunc))
+	customClientFunc := func(network, addr string) (radix.Client, error) {
+
+		customClientConnFunc := func(network, addr string) (radix.Conn, error) {
+			return radix.Dial(network, addr,
+				// radix.DialTimeout(1*time.Second),
+				radix.DialAuthPass("123"),
+			)
+		}
+
+		return radix.NewPool(network, addr, 100, radix.PoolConnFunc(customClientConnFunc))
+	}
+
+	sentinel, err := radix.NewSentinel("mymaster", address, radix.SentinelConnFunc(customConnFunc), radix.SentinelPoolFunc(customClientFunc))
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	m, s := sentinel.Addrs()
-	fmt.Println(m)
-	fmt.Println(s)
+	// fmt.Println(sentinel.SentinelAddrs())
+	_, slaves := sentinel.Addrs()
+	conn, err := radix.Dial("tcp", slaves[len(slaves)-1], radix.DialAuthPass("123"))
+	ps := radix.PubSub(conn)
+	if err != nil {
+		fmt.Println(err)
+	}
+	msgCh := make(chan radix.PubSubMessage)
+	if err := ps.PSubscribe(msgCh, "*"); err != nil {
+		fmt.Println(err)
+	}
+	for msg := range msgCh {
+		log.Printf("publish to channel %q received: %q", msg.Channel, msg.Message)
+	}
+	// client, err := sentinel.Client(slaves[0])
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// var s string
+	// err = client.Do(radix.Cmd(&s, "INFO"))
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// fmt.Println(s)
+
 }
--- test.go
+++ test.go
@@ -1,26 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"time"
+// import (
+// 	"fmt"
+// 	"time"
 
-	"github.com/go-redis/redis"
-)
+// 	"github.com/go-redis/redis"
+// )
 
-func main() {
-	redisdb := redis.NewFailoverClient(&redis.FailoverOptions{
-		MasterName:       "mymaster",
-		SentinelAddrs:    []string{":26379", ":26380", "26381"},
-		SentinelPassword: "12345678",
-		Password:         "123",
-	})
+// func main() {
+// 	redisdb := redis.NewFailoverClient(&redis.FailoverOptions{
+// 		MasterName:       "mymaster",
+// 		SentinelAddrs:    []string{":26379", ":26380", "26381"},
+// 		SentinelPassword: "12345678",
+// 		Password:         "123",
+// 	})
 
-	for {
-		valInfo, err := redisdb.Do("ROLE").Result()
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(valInfo)
-		time.Sleep(10 * time.Second)
-	}
-}
+// 	for {
+// 		valInfo, err := redisdb.Do("ROLE").Result()
+// 		if err != nil {
+// 			fmt.Println(err)
+// 		}
+// 		fmt.Println(valInfo)
+// 		time.Sleep(10 * time.Second)
+// 	}
+// }

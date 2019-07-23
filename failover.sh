a7d3276ea0555e283507ab39b17422a4f39dc1e3
yushizhao <yushizhao@outlook.com>
2019-07-23 08:20:21 +0000

    slave vip

--- failover.sh
+++ failover.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
 MASTER_IP=${6}
+OLD_IP=${4}
 MY_IP='172.16.212.121'   # 各サーバ自身のIP
 VIP='172.16.212.124'     # VIP
+SLAVEVIP=
 NETMASK='24'          # Netmask
 INTERFACE='ens160'      # インターフェイス
 
 if [ ${MASTER_IP} = ${MY_IP} ]; then
         sudo /sbin/ip addr add ${VIP}/${NETMASK} dev ${INTERFACE}
         sudo /sbin/arping -q -c 3 -A ${VIP} -I ${INTERFACE}
-        exit 0
+        sudo /sbin/ip addr del ${SLAVEVIP}/${NETMASK} dev ${INTERFACE}
 else
         sudo /sbin/ip addr del ${VIP}/${NETMASK} dev ${INTERFACE}
-        exit 0
 fi
-exit 1
+
+if [ ${OLD_IP} != ${MY_IP} ] && [ ${MASTER_IP} != ${MY_IP} ]; then
+        sudo /sbin/ip addr add ${SLAVEVIP}/${NETMASK} dev ${INTERFACE}
+        sudo /sbin/arping -q -c 3 -A ${SLAVEVIP} -I ${INTERFACE}
---
+++ more-than-notify.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+SELFIP=
+SLAVEVIP=
+NETMASK='24'    
+INTERFACE='ens160'
+
+if [ ${1} == "+sdown" ]; then
+  read -r -a array <<< "${2}"
+  role=${array[0]}
+  ip=${array[2]}
+  masterip=${array[6]}
+
+  if [ ${role} == "slave" ] && [ ${masterip} != ${SELFIP} ]; then
+    if [ ${ip} == ${SELFIP} ]; then
+      sudo /sbin/ip addr del ${SLAVEVIP}/${NETMASK} dev ${INTERFACE}
+    else
+      # sudo /sbin/ip addr add ${SLAVEVIP}/${NETMASK} dev ${INTERFACE}
+      sudo /sbin/arping -q -c 3 -A ${SLAVEVIP} -I ${INTERFACE}
+    fi
+  fi
+fi
+
+if [ ${1} == "-sdown" ]; then
+  read -r -a array <<< "${2}"
+  role=${array[0]}
+  ip=${array[2]}
+  masterip=${array[6]}
+
+  if [ ${role} == "slave" ] && [ ${masterip} != ${SELFIP} ]; then
+    if [ ${ip} == ${SELFIP} ]; then
+      sudo /sbin/ip addr add ${SLAVEVIP}/${NETMASK} dev ${INTERFACE}
+    fi
+  fi
+fi
+
+
+# msg="{\"msgtype\": \"text\", 
+#         \"text\": {
+#              \"content\": \"${1} ${2}\"
+#         }
+#       }"
+
+# echo ${msg}
+# curl 'https://oapi.dingtalk.com/robot/send?access_token=0789c47ae457d7198adf27d03af3a44d3fb2dd8190a6320daa2fc978d1bcba11' \
+#    -H 'Content-Type: application/json' \
+#    -d "${msg}"

8aae8a13bec99461122eecf7cd2f752402337358
yushizhao <yushizhao@outlook.com>
2019-07-25 07:00:14 +0000

    fix

--- start-sentinel.sh
+++ start-sentinel.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 nohup redis-sentinel.sh sentinel.conf > nohup.out 2>&1 &
-tail -f nohup.out
--- start-server.sh
+++ start-server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 nohup redis-server redis.conf > nohup.out 2>&1 &
-tail -f nohup.out
